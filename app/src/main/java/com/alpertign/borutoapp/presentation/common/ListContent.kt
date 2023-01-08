768e847dcff4eeb9bde2a3b2c6bf3ba7cad46cf5
Alperen Acikgoz <alpertign@gmail.com>
2023-01-08 19:23:25 +0300

    - HeroItem created

---
+++ app/src/main/java/com/alpertign/borutoapp/presentation/common/ListContent.kt
@@ -0,0 +1,166 @@
+package com.alpertign.borutoapp.presentation.common
+
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import androidx.paging.compose.LazyPagingItems
+import coil.annotation.ExperimentalCoilApi
+import coil.compose.rememberImagePainter
+import com.alpertign.borutoapp.R
+import com.alpertign.borutoapp.domain.model.Hero
+import com.alpertign.borutoapp.navigation.Screen
+import com.alpertign.borutoapp.presentation.components.RatingWidget
+import com.alpertign.borutoapp.ui.theme.*
+import com.alpertign.borutoapp.util.Constants.BASE_URL
+
+/**
+ * Created by Alperen Acikgoz on 08,January,2023
+ */
+
+@Composable
+fun ListContent(
+    heroes: LazyPagingItems<Hero>,
+    navController: NavHostController
+) {
+
+}
+
+@ExperimentalCoilApi
+@Composable
+fun HeroItem(
+    hero: Hero,
+    navController: NavHostController
+) {
+
+    val painter = rememberImagePainter(data = "$BASE_URL${hero.image}") {
+        placeholder(R.drawable.placeholder)
+        error(R.drawable.placeholder)
+    }
+
+    Box(modifier = Modifier
+        .height(HERO_ITEM_HEIGHT)
+        .clickable {
+            navController.navigate(Screen.Details.passHeroId(heroId = hero.id))
+        },
+        contentAlignment = Alignment.BottomStart
+        ) {
+        Surface(shape = Shapes.large) {
+            Image(
+                modifier = Modifier.fillMaxSize(),
+                painter = painter,
+                contentDescription = stringResource(R.string.hero_image),
+                contentScale = ContentScale.Crop
+            )
+        }
+
+        Surface(
+            modifier = Modifier
+                .fillMaxHeight(0.4f)
+                .fillMaxWidth(),
+            color = Color.Black.copy(alpha = ContentAlpha.medium),
+            shape = RoundedCornerShape(
+                bottomStart = LARGE_PADDING,
+                bottomEnd = LARGE_PADDING
+            )
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(all = MEDIUM_PADDING)
+            ) {
+                Text(
+                    text = hero.name,
+                    color = MaterialTheme.colors.topAppBarContentColor,
+                    fontSize = MaterialTheme.typography.h5.fontSize,
+                    fontWeight = FontWeight.Bold,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
+                )
+                Text(
+                    text = hero.about,
+                    color = Color.White.copy(alpha = ContentAlpha.medium),
+                    fontSize = MaterialTheme.typography.subtitle1.fontSize,
+                    fontWeight = FontWeight.Bold,
+                    maxLines = 3,
+                    overflow = TextOverflow.Ellipsis
+                )
+                Row(
+                    modifier = Modifier.padding(top = SMALL_PADDING),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    RatingWidget(
+                        modifier = Modifier.padding(end = SMALL_PADDING),
+                        rating = hero.rating
+                    )
+                    Text(
+                        text = "(${hero.rating})",
+                        textAlign = TextAlign.Center,
+                        color = Color.White.copy(alpha = ContentAlpha.medium)
+                        )
+                }
+            }
+
+        }
+
+    }
+
+}
+
+
+@ExperimentalCoilApi
+@Composable
+@Preview
+fun HeroItemPreview(){
+    HeroItem(
+        hero = Hero(
+            id = 1,
+            name = "Sasuke",
+            image = "",
+            about = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+            rating = 4.5,
+            power = 100,
+            month = "",
+            day = "",
+            family = listOf(),
+            abilities = listOf(),
+            natureTypes = listOf()
+        ), navController = rememberNavController())
+}
+
+@ExperimentalCoilApi
+@Composable
+@Preview(uiMode = UI_MODE_NIGHT_YES)
+fun HeroItemDarkPreview(){
+    HeroItem(
+        hero = Hero(
+            id = 1,
+            name = "Sasuke",
+            image = "",
+            about = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+            rating = 4.5,
+            power = 100,
+            month = "",
+            day = "",
+            family = listOf(),
+            abilities = listOf(),
+            natureTypes = listOf()
+        ), navController = rememberNavController())
+}
+
+
+
--- app/src/main/java/com/alpertign/borutoapp/presentation/screens/home/HomeScreen.kt
+++ app/src/main/java/com/alpertign/borutoapp/presentation/screens/home/HomeScreen.kt
@@ -1,14 +1,11 @@
 package com.alpertign.borutoapp.presentation.screens.home
 
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import androidx.paging.compose.collectAsLazyPagingItems
-import com.alpertign.borutoapp.presentation.components.RatingWidget
-import com.alpertign.borutoapp.ui.theme.LARGE_PADDING
+import com.alpertign.borutoapp.presentation.common.ListContent
 
 /**
  * Created by Alperen Acikgoz on 05,January,2023
@@ -18,16 +15,19 @@ import com.alpertign.borutoapp.ui.theme.LARGE_PADDING
 fun HomeScreen(
     navController: NavHostController,
     homeViewModel: HomeViewModel = hiltViewModel()
-    ) {
+) {
     val allHeroes = homeViewModel.getAllHeroes.collectAsLazyPagingItems()
 
     Scaffold(
         topBar = {
             HomeTopBar(onSearchClicked = {})
+        },
+        content = {
+            ListContent(
+                heroes = allHeroes,
+                navController = navController
+            )
         }
-    ) {
-        RatingWidget(modifier = Modifier.padding(all = LARGE_PADDING), rating = 4.1)
-
-    }
+    )
 
 }
