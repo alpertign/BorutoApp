9d75787de2cf4822a8c206fc44d367fa7965809f
Alperen Acikgoz <alpertign@gmail.com>
2023-01-08 16:16:44 +0300

    - Custom Rating Widget created

--- app/src/main/java/com/alpertign/borutoapp/presentation/screens/home/HomeScreen.kt
+++ app/src/main/java/com/alpertign/borutoapp/presentation/screens/home/HomeScreen.kt
@@ -1,10 +1,14 @@
 package com.alpertign.borutoapp.presentation.screens.home
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import androidx.paging.compose.collectAsLazyPagingItems
+import com.alpertign.borutoapp.presentation.components.RatingWidget
+import com.alpertign.borutoapp.ui.theme.LARGE_PADDING
 
 /**
  * Created by Alperen Acikgoz on 05,January,2023
@@ -22,7 +26,7 @@ fun HomeScreen(
             HomeTopBar(onSearchClicked = {})
         }
     ) {
-
+        RatingWidget(modifier = Modifier.padding(all = LARGE_PADDING), rating = 4.1)
 
     }
 
